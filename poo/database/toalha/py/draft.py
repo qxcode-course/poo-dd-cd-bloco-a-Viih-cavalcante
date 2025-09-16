46a28f9e1e725daf8b4a88584859fed3c84ba71c
Viih Cavalcante <viih.cavalcante@alu.ufc.br>
2025-09-16 20:24:56 +0000

    fazendo atv da toalha

---
+++ poo/database/toalha/py/draft.py
@@ -0,0 +1,13 @@
+class Towel:
+    def __init__(self)
+        self.color = color
+        self.size = size
+        self.wetness = 0
+
+
+
+towel = Towel("Green", "G" )
+towel = Towel ("yellow", "G")
+outra = toalha
+toalha.color = "blue"
+toalha.color = "blue"
